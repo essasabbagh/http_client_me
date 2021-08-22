f13560f788f2e0aa280a1c518423e5e208c708dc
essasabbagh <essasabbagh@gmail.com>
2021-08-22 09:55:31 +0300

    Add sqlite test

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http_client/controller/test_controller.dart';
+
 import 'package:get/get.dart';
-import 'package:http_client/test_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,11 +12,38 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       debugShowCheckedModeBanner: false,
       home: TestPage(),
+    );
+  }
+}
+
+class TestPage extends StatelessWidget {
+  final controller = TestController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('TestPage')),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                controller.getData();
+              },
+              child: Text('GET Data', style: TextStyle(fontSize: 24)),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                controller.postData();
+              },
+              child: Text('POST Data', style: TextStyle(fontSize: 24)),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
